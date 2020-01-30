14ca9ff591acd1e4cb1c90367ec0ada41cd54b6c
Sadi009 <onlyfriends222@gmail.com>
2020-01-30 15:42:27 +0600

    test

--- src/app/hospital/hospital-list/details/details.page.ts
+++ src/app/hospital/hospital-list/details/details.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-details',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage implements OnInit {
+  public isMenuOpen: boolean = false;
+  details;
+  clickCounter = 0;
+  cateSlide = {
+    initialSlide: 1,
+    slidesPerView: 1,
+    centeredSlides: true,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: true,
+    pagination: {
+      clickable: true
+    }
+  };
 
-  constructor() { }
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.details = params;
+    });
+  }
+
+  public toggleAccordion(): void {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 
 }
